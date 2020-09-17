d2231efe16c00419f975114b7184ab114b4127f4
DanielGestur <danielt20@ru.is>
2020-09-17 18:21:42 +0000

    More Algorithm

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,5 +1,17 @@
 
 # Markmið að komast á reit 3,1
 # Gera fall fyrir reiti
+
+    # 1,1 - Bara hægt að fara Norður
+    # 1,2 - Hægt að fara Norður, Austur og Suður
+    # 1,3 - Hægt að fara Suður og Austur
+    # 2,1 - Bara hægt að fara Norður
+    # 2,2 - Hægt að fara Vestur og Suður
+    # 2,3 - Hægt að fara Vestur og Austur
+    # 3,1 - Bara hægt að fara Norður
+    # 3,2 - Hægt að fara Norður og Suður
+    # 3,3 - Hægt að fara Vestur og Suður
+
 # í upphafi þarf að prenta út í hvaða átt er hægt að fara "You can travel: (N)orth".
 # Ef hreying er ekki möguleg á að prentast út "Not a valid direction!"
+#
