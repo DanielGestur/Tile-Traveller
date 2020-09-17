b8e9c66bde4dd9020b89f6d8b6ad5f2bded228a1
DanielGestur <danielt20@ru.is>
2020-09-17 18:07:29 +0000

    First algorithm

---
+++ TileTraveller.py
@@ -0,0 +1,5 @@
+
+# Markmið að komast á reit 3,1
+# Gera fall fyrir reiti
+# í upphafi þarf að prenta út í hvaða átt er hægt að fara "You can travel: (N)orth".
+# Ef hreying er ekki möguleg á að prentast út "Not a valid direction!"
